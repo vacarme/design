5a7babcbe72c131b83bdbc5495d0f6f6c2ad8cdb
vacarme <ironex@gmail.com>
2024-06-20 16:36:45 +0200

    refactor(factory): added type hints.

--- factory.py
+++ factory.py
@@ -1,11 +1,19 @@
-from abc import ABC, abstractmethod, abstractstaticmethod
+"""
+Factory.
+
+When to use :
++ when you don't know beforehand the exact objects your code should work with. (Need a new animal just add a new constructor subclass.)
++ save system resources by reusing existing objects instead of rebuilding them (pooling).
+"""
+
+from abc import ABC, abstractmethod
 
 
 # Target objects we want to give to the client code.
 class Animal(ABC):
     """Interface for products."""
 
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
 
     @abstractmethod
@@ -24,7 +32,7 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name, human) -> None:
+    def __init__(self, name: str, human) -> None:
         super().__init__(name)
         self.human = human
 
@@ -36,9 +44,8 @@ class Dog(Animal):
 
 
 # Factories
-
-
-def factory_method(animal: str, name: str, **kwargs):
+## Simple Factory Function
+def factory_method(animal: str, name: str, **kwargs) -> Animal:
     """
     Simple Function
 
@@ -59,9 +66,24 @@ class Factory(ABC):
     def __init__(self) -> None: ...
     def create_animal(self) -> Animal: ...
 
+    def make_animal_speak(self) -> None:
+        """
+        Make the animal speak.
+
+        The creator's primary responsibility is not creating products.
+        It usually contains some core business logic that relies
+        on product objects returned by the factory method.
+
+        Subclasses can indirectly change that business logic by overriding the factory method
+        and returning a different type of product from it.
+
+        """
+        animal = self.create_animal()
+        animal.make_sound()
+
 
 class DogFactory(Factory):
-    def __init__(self, name, human) -> None:
+    def __init__(self, name: str, human) -> None:
         self.name = name
         self.human = human
 
@@ -70,7 +92,7 @@ class DogFactory(Factory):
 
 
 class CatFactory(Factory):
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def create_animal(self) -> Cat:
@@ -79,10 +101,12 @@ class CatFactory(Factory):
 
 if __name__ == "__main__":
     # client code getting a dog using the simple function:
-    animal = factory_method(animal="dog", name="Billy", **{"human": "John"})
+    animal = factory_method(
+        animal="dog", name="Billy", **{"human": "John"}
+    )  # inferred type is Animal
     animal.make_sound()
 
     # client code getting a dog using the concrete factory
     animal_factory = DogFactory(name="Billy", human="John")
-    animal = animal_factory.create_animal()
+    animal = animal_factory.create_animal()  # inferred type is Dog
     animal.make_sound()
